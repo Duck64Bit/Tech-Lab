6198b9e14a452c263aab7fe09bd050c16957a1f2
Duck64Bit <duck.64.bit@gmail.com>
2023-09-10 07:50:44 +0500

    910 New Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,15 +11,9 @@ const Footer = () => {
       <div>
         <h5>Follow Us</h5>
         <div>
-          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
-            Youtube
-          </a>
-          <a href="https://instagram.com/meabhisingh" target={"blank"}>
-            Instagram
-          </a>
-          <a href="https://github.com/meabhisingh" target={"blank"}>
-            Github
-          </a>
+          <a>Youtube</a>
+          <a>Instagram</a>
+          <a>Github</a>
         </div>
       </div>
     </footer>
